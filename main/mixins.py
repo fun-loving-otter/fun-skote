1d6fa6622293035bdd118c8ddb0a55dc848a58a3
oleg20111511 <oleg20111511@list.ru>
2023-07-01 14:17:59 +0300

    Change package choice

--- main/mixins.py
+++ main/mixins.py
@@ -1,9 +1,6 @@
-from django.urls import reverse_lazy
-
 from payments.mixins import SubscriptionRequiredMixin
 from payments.models import Subscription
 
 
 class DataPackageRequiredMixin(SubscriptionRequiredMixin):
     subscription_queryset = Subscription.objects.exclude(package__datapackagebenefits=None)
-    subscription_does_not_exist_url = reverse_lazy('main:buy-package')

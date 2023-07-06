543b5cdbe7112830d6f38db475bd4ce2fdebe589
oleg20111511 <oleg20111511@list.ru>
2023-07-07 00:19:32 +0300

    Fix previous fix

--- main/views/data/data.py
+++ main/views/data/data.py
@@ -53,7 +53,7 @@ class DataAPIListView(ListAPIView):
     def get_queryset(self):
         q = Data.objects.all()
         if connection.vendor == 'postgresql':
-            q = q.order_by('-pk', 'organization_name').distinct('organization_name')
+            q = q.order_by('organization_name', '-pk').distinct('organization_name')
         else:
             pks_q = q.values('organization_name').annotate(max_pk=Max('pk')).values('max_pk')
             q = q.filter(pk__in=pks_q)
